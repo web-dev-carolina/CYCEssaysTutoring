5db71ea7a493190ad14320c65ce307448958c284
Roderick Liu <xinrod@live.unc.edu>
2020-10-13 20:36:18 -0400

    update book image on home page

--- cyc-essays-tutoring/src/App.js
+++ cyc-essays-tutoring/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import NavbarContainer from './components/NavbarContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './pexels-caio-46274.jpg';
 
 const App = () => {
   return (
