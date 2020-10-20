373948da37649d0e58811081a6e21352816ac76e
Arpita Das <arpdas16@gmail.com>
2020-10-20 13:37:17 -0400

    Revert "update book image on home page"
    This reverts commit 5db71ea7a493190ad14320c65ce307448958c284.

--- cyc-essays-tutoring/src/App.js
+++ cyc-essays-tutoring/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavbarContainer from './components/NavbarContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './pexels-caio-46274.jpg';
 
 const App = () => {
   return (
