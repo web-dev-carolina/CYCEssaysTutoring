32f4d545b045eea216f69db4e57387cdfcb90ea6
Daniel <danieloh@live.unc.edu>
2020-10-23 20:52:02 -0400

    testimonials

--- cyc-essays-tutoring/src/components/Testimonials.jsx
+++ cyc-essays-tutoring/src/components/Testimonials.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Testimonials = () => {
     return(
-        <h2 id="testimonial"> Testimonials </h2>
+        <div className="home-section-wrap">
+            <h2 className="section-heading">Testimonials</h2>
+            <img src="testimonials1.jpg" alt="Testimonial1"/>
+            <br></br>
+            <p className="paragraph-light">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+            Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis 
+            parturient montes, nascetur ridiculus 
+            mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+        </div>
     );
 }
 
