3ed1cfc589f6fe18aacaceafe24c4bec96e86cdc
Roderick Liu <xinrod@live.unc.edu>
2020-11-08 21:02:59 -0500

    added contact form functionality

--- cyc-essays-tutoring/src/components/Contact.jsx
+++ cyc-essays-tutoring/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap'
 import { Row, Container, Col } from 'react-bootstrap';
 import './contact.css'
 
-const Contact = () => {
+const Contact = ({handleSubmit}) => {
     return (
         <Container>
         <div className="container mb-5 row mr-auto ml-auto contact-form">
@@ -19,16 +19,16 @@ const Contact = () => {
             </div>
             <div className="col-xs-12 col-md-6">
                 <div className="pg1-2-txt">
-                <Form>
+                <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formBasicName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="email" placeholder="Enter your name" />
+                        <Form.Control type="name" placeholder="Enter your name" />
                     </Form.Group>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
                     </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Group controlId="controlTextarea1">
                         <Form.Label>Message</Form.Label>
                         <Form.Control as="textarea" type="message" placeholder="Enter your message here" />
                     </Form.Group>
