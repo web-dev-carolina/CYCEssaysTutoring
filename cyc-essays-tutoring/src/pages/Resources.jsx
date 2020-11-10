4289040c9ebb8035cedbee56823a8e7a2a7bb079
Arpita Das <arpitad@live.unc.edu>
2020-11-10 14:51:46 -0500

    added course info, news articles (need to link)

--- cyc-essays-tutoring/src/pages/Resources.jsx
+++ cyc-essays-tutoring/src/pages/Resources.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../styles/Resources.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
-
+import '../styles/sat_cover.pdf';
 const Resources = () => {
     return (
         <div className="resources-container container">
@@ -29,29 +29,31 @@ const Resources = () => {
             <Row>
                 <div className="card-deck">
                     <div className="card shadow" onClick="handleFirstResource">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/5/50/Closed_Book_Icon.svg" alt="File:Closed Book Icon.svg"/>
                         <div className="card-body">
                             <h5 className="card-title">Textbook: Ace the SAT Essay</h5>
                             <p className="card-text">How to master the timed writing process by Dana L. Lebo, PHD</p>
-                            <a className="text-right" href="">See textbook cover...</a>
+                            <a className="text-right" href="https://www.amazon.com/Ace-SAT-Essay-Writing-Process/dp/1934442380/ref=cm_cr_pr_product_top" target="_blank">Textbook link...</a> 
                         </div>
                         <div className="card-footer">
                         </div>
                     </div>
                     <div className="card shadow">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/0/02/201807_book_B.svg" alt="File:201807 book B.svg" />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <h5 className="card-title">Course Material: Binder Inserts</h5>
+                            <p className="card-text">Everything you need for the virtual workshop from example essay questions to detailed material. Please see the link below to view the pdf.</p>
+                            <a className="text-right" href="../styles/binder_inserts.pdf" target="_blank">View PDF...</a> 
                         </div>
                         <div className="card-footer">
                         </div>
                     </div>
                     <div className="card shadow">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/8/87/Magnifying_glass_icon_mgx2.svg" alt="File:Magnifying glass icon mgx2.svg" />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <h5 className="card-title">Virtual Workshop Overview</h5>
+                            <p className="card-text">For the first time in history, most colleges have dropped the requirement for standardized test scores.  Due to the pandemic, they are turning instead to the personal essay as one of the biggest factors in college admission...</p>
+                            <a className="text-right" href="../styles/workshop_desc.pdf" target="_blank">Read more...</a> 
                         </div>
                         <div className="card-footer">
                         </div>
@@ -74,19 +76,21 @@ const Resources = () => {
                         </div>
                     </div>
                     <div className="card shadow">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/1/19/%D0%9C%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D0%B0.07.svg" alt="File: Woman.svg" />
                         <div className="card-body">
                             <h5 className="card-title">Writing the Essay: Sound Advice from an Expert</h5>
                             <p className="card-text">By Parke Muth, Assistant Dean of Admission, University of Virginia</p>
+                            <a className="text-right" href="..." target="_blank">Read more...</a> 
                         </div>
                         <div className="card-footer">
                         </div>
                     </div>
                     <div className="card shadow">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/5/57/130-man-student-2.svg" alt="File:130-man-student-2.svg" />
                         <div className="card-body">
                             <h5 className="card-title">Workshop Helps Students Write Winning College Applications</h5>
                             <p className="card-text">And Build Competency in a Much-Valued Leadership Skill</p>
+                            <a className="text-right" href="..." target="_blank">Read more...</a>
                         </div>
                         <div className="card-footer">
                         </div>
