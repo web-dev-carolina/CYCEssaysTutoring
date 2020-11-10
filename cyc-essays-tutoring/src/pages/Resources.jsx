91dfe7e654afcf4356ebd0c1602a69896fd76155
Arpita Das <arpitad@live.unc.edu>
2020-11-09 22:05:10 -0500

    minor card footer change

--- cyc-essays-tutoring/src/pages/Resources.jsx
+++ cyc-essays-tutoring/src/pages/Resources.jsx
@@ -36,7 +36,6 @@ const Resources = () => {
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                     <div className="card">
@@ -46,7 +45,6 @@ const Resources = () => {
                             <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                     <div className="card">
@@ -56,7 +54,6 @@ const Resources = () => {
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                 </div>
@@ -74,7 +71,6 @@ const Resources = () => {
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                     <div className="card">
@@ -84,7 +80,6 @@ const Resources = () => {
                             <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                     <div className="card">
@@ -94,7 +89,6 @@ const Resources = () => {
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                         </div>
                         <div className="card-footer">
-                            <small className="text-muted">Last updated 3 mins ago</small>
                         </div>
                     </div>
                 </div>
