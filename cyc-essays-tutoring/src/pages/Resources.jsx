e4e243340b68198d4d643e56970d5a025cb10fa5
Arpita Das <arpitad@live.unc.edu>
2020-11-09 22:01:38 -0500

    resources layout update

--- cyc-essays-tutoring/src/pages/Resources.jsx
+++ cyc-essays-tutoring/src/pages/Resources.jsx
@@ -3,79 +3,105 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../styles/Resources.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 
 const Resources = () => {
     return (
-        <Container className="resources-container">
-            <Row>
-                <Col className="col-4">
-                    <div>
-                    <img className="computer" src="https://upload.wikimedia.org/wikipedia/commons/7/70/Hands_typing_on_white_laptop_scene.svg" alt="File:Hands typing on white laptop scene.svg"></img>
+        <div className="resources-container container">
+            <div className="resource-heading resource-section container-fluid">
+            <Row className="title-header">
+                <div>
+                    <Col className="inline-x col-4">
+                    <div className="image-container">
+                        <img className="computer" src="https://upload.wikimedia.org/wikipedia/commons/7/70/Hands_typing_on_white_laptop_scene.svg" alt="File:Hands typing on white laptop scene.svg"></img>
                     </div>
-                </Col>
-                <Col className="col-8">
-                    <div className="resource-text">
-                    <Row>
-                        <h1>Resources</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus - 
-description of what types of resources you are delivering here, maybe link a google drive folder with articles, etc. that you think are valuable</p>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Row>
-                                <div className="tile">
-                                    <div className="icondiv">
-                                        <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Pencil_icon_vector.svg" alt="Craig Creeger, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"></img>
-                                    </div>
-                                    <div className="fileMain">
-                                        <h4>File Title</h4>
-                                        <p>File info: Lorem ipsum - short description of item </p> 
-                                    </div>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className="tile">
-                                    <div className="icondiv">
-                                        <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Pencil_icon_vector.svg" alt="Craig Creeger, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"></img>
-                                    </div>
-                                    <div className="fileMain">
-                                        <h4>File Title</h4>
-                                        <p>File info: Lorem ipsum - short description of item </p> 
-                                    </div>
-                                </div>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <div className="tile">
-                                    <div className="icondiv">
-                                        <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Pencil_icon_vector.svg" alt="Craig Creeger, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"></img>
-                                    </div>
-                                    <div className="fileMain">
-                                        <h4>File Title</h4>
-                                        <p>File info: Lorem ipsum - short description of item </p> 
-                                    </div>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div className="tile">
-                                    <div className="icondiv">
-                                        <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Pencil_icon_vector.svg" alt="Craig Creeger, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"></img>
-                                    </div>
-                                    <div className="fileMain">
-                                        <h4>File Title</h4>
-                                        <p>File info: Lorem ipsum - short description of item </p> 
-                                    </div>
-                                </div>
-                            </Row>
-                        </Col>
-                    </Row>
-                    </div>
-                    
-                </Col>
+                    </Col>
+                    <Col className="inline-x col-8">
+                        <div className="resource-text">
+                            <h1 className="display-1 font-weight-normal">Resources</h1>
+                        </div>
+                    </Col>
+                </div>
             </Row>
-        </Container>
+            </div>
+            <div className="course-resources resource-section container-fluid">
+            <h1 className="display-4 text-center font-weight-light">Course Resources</h1>
+            <Row>
+                <div className="card-deck">
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+
+            </Row>
+            </div>
+            <div className="news-container resource-section container-fluid">
+            <h1 className="display-4 text-center font-weight-light">News Articles</h1>
+            <Row>
+                <div className="card-deck">
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img className="card-img-top" src="..." alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+
+            </Row>
+            </div>
+        </div>
     );
 }
 
