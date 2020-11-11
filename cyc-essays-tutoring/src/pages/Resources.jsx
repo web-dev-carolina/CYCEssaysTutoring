c8843ea151cd8a452747e057bd075c6368af1d9d
Adysen <adysen66@gmail.com>
2020-11-10 20:31:16 -0500

    reorder resources

--- cyc-essays-tutoring/src/pages/Resources.jsx
+++ cyc-essays-tutoring/src/pages/Resources.jsx
@@ -110,15 +110,7 @@ const Resources = () => {
                         <div className="card-footer">
                         </div>
                     </div>
-                    <div className="card shadow">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                            <div className="card-footer">
-                            </div>
-                    </div>
+                    
                     <div className="card shadow">
                         <img className="card-img-top" src="..." alt="Card image cap" />
                         <div className="card-body">
