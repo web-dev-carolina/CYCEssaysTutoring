ad0a23b8b711905fb47e64e6e8dc6abdd36d16fa
Roderick Liu <xinrod@live.unc.edu>
2020-12-03 13:59:45 -0500

    fix config file

--- cyc-essays-tutoring/src/firebase/firebaseConfig.js
+++ cyc-essays-tutoring/src/firebase/firebaseConfig.js
@@ -8,7 +8,7 @@ import Axios from 'axios'
 let config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: "cycessays",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
